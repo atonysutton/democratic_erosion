6313a419c45ea84b63213caa89840c9ce50d44b8
Tony Sutton <as6031a@student.american.edu>
2022-01-19 14:22:24 -0500

    point script at new data file

--- democratic_erosion.R
+++ democratic_erosion.R
@@ -1,4 +1,5 @@
 setwd('C:/Tony/git_workspace/democratic_erosion')
+#setwd('C:/Tony/Political Science MA/_thesis/quantitative work - backup copy/democratic_erosion')
 
 #load libraries
 library(tidyverse)
@@ -6,4 +7,19 @@ library(tidyverse)
 #load and shape data ----
 
 ##regime type data from V-Dem - one row per country year 
-vdem <- read_csv('./data/V-Dem-CY-Full+Others-v11.1.csv')
+#vdem <- read_csv('./data/V-Dem-CY-Full+Others-v11.1.csv')
+
+#vdem <- vdem %>% select(country_name, country_text_id, year, COWcode,
+#                        v2x_polyarchy, v2x_libdem, v2x_partipdem, v2x_delibdem, v2x_egaldem,
+#                        v2xnp_client, v2elvotbuy, v2dlencmps, v2psprlnks, v2x_elecreg,
+#                        v2smonex, v2smmefra, v2smgovdom, v2smpardom, v2smfordom, 
+#                        v2cacamps, v2smpolsoc, v2smpolhate,
+#                        e_migdpgro, e_migdppc,
+#                        e_mipopula, e_miurbani,
+#                        e_total_fuel_income_pc, e_total_resources_income_pc)
+
+#write_csv(vdem, file = './data/vdem_trimmed.csv')
+
+vdem <- read_csv('./data/vdem_trimmed.csv')
+
+skimr::skim(vdem)
