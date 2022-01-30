c96669eeb3b7f39563ac2e4b8aae46ce8f9e81e5
Tony Sutton <as6031a@student.american.edu>
2022-01-30 16:54:11 -0500

    begin identifying democratic spells

--- democratic_erosion.R
+++ democratic_erosion.R
@@ -22,4 +22,58 @@ library(tidyverse)
 
 vdem <- read_csv('./data/vdem_trimmed.csv')
 
+vdem <- vdem %>% filter(!is.na(v2x_polyarchy))
+
 skimr::skim(vdem)
+
+#set arbitrary thresholds
+dem_threshold = 0.5
+
+#identify consolidated democracies ----
+
+##label democratization and autocratization, which define spells
+ ###create empty variable
+vdem$democratize <- as.logical(FALSE)
+
+for (i in seq_along(vdem$year)){
+  #skip along if no entry for the country in the prior year
+  if (length(vdem$v2x_polyarchy[vdem$country_name == vdem[i,]$country_name & vdem$year == (vdem[i,]$year - 1)]) == 0) next
+  
+  #identify the polyarchy score for the same country in the preceding year
+  pre1_polyarchy <- vdem %>%
+    filter(country_name == vdem[i,]$country_name & year == (vdem[i,]$year - 1)) %>%
+    pull(v2x_polyarchy)
+  
+  #call it democratization if a country is a democracy this year and was not in prior year
+  vdem[i,]$democratize <- 
+    case_when(pre1_polyarchy > dem_threshold ~ FALSE,
+              vdem[i,]$v2x_polyarchy >= dem_threshold ~ TRUE,
+              TRUE ~ FALSE
+              )
+}
+
+summary(vdem$democratize)
+
+####still need to address instance of a country appearing for the first year in the df but as a democracy ####
+
+
+##how? 
+ ##minimum polyarchy threshold? perhaps look for discontinuities in polyarchy peak among all that ever eroded.
+ ##robust across multiple vdem high level indexes?
+ ##longevity threshold, perhaps empirically derived from survival distribution?
+ ##some combination?
+ ##or come at it backwards, as requiring two-step erosion? would then be identifying a phenomenon, not a dependent variable.
+
+
+
+##of consolidated pool, label whether later eroded
+
+#test whether theorized factors predict later erosion ----
+
+##clientelism
+
+##information control
+
+##polarization
+
+##search for most telling time lag
