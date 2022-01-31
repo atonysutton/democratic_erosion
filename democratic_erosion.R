f1c4737ff537cdf1761d9f567e28e620b3d2a4d0
Tony Sutton <as6031a@student.american.edu>
2022-01-30 20:57:10 -0500

    label autocracy onsets

--- democratic_erosion.R
+++ democratic_erosion.R
@@ -9,6 +9,7 @@ library(tidyverse)
 ##regime type data from V-Dem - one row per country year 
 #vdem <- read_csv('./data/V-Dem-CY-Full+Others-v11.1.csv')
 
+##trim to a manageable file size by selecting only relevent variables
 #vdem <- vdem %>% select(country_name, country_text_id, year, COWcode,
 #                        v2x_polyarchy, v2x_libdem, v2x_partipdem, v2x_delibdem, v2x_egaldem,
 #                        v2xnp_client, v2elvotbuy, v2dlencmps, v2psprlnks, v2x_elecreg,
@@ -32,27 +33,52 @@ dem_threshold = 0.5
 #identify consolidated democracies ----
 
 ##label democratization and autocratization, which define spells
- ###create empty variable
-vdem$democratize <- as.logical(FALSE)
-
+ ###create empty variables
+vdem$democratize <- as.logical(NA)
+vdem$autocratize <- as.logical(NA)
+vdem$first_appear <- as.logical(FALSE)
+ 
+ ###loop over every country-year
 for (i in seq_along(vdem$year)){
-  #skip along if no entry for the country in the prior year
-  if (length(vdem$v2x_polyarchy[vdem$country_name == vdem[i,]$country_name & vdem$year == (vdem[i,]$year - 1)]) == 0) next
+  #if no entry for the country in the prior year, label first year and skip to next country-year
+  if (length(vdem$v2x_polyarchy[vdem$country_name == vdem$country_name[i] & vdem$year == (vdem$year[i] - 1)]) == 0) vdem$first_appear[i] <- TRUE
+  if (vdem$first_appear[i] == TRUE) next
   
-  #identify the polyarchy score for the same country in the preceding year
+  #otherwise, identify the polyarchy score for the same country in the preceding year
   pre1_polyarchy <- vdem %>%
-    filter(country_name == vdem[i,]$country_name & year == (vdem[i,]$year - 1)) %>%
+    filter(country_name == vdem$country_name[i] & year == (vdem$year[i] - 1)) %>%
     pull(v2x_polyarchy)
   
   #call it democratization if a country is a democracy this year and was not in prior year
-  vdem[i,]$democratize <- 
+  vdem$democratize[i] <- 
     case_when(pre1_polyarchy > dem_threshold ~ FALSE,
-              vdem[i,]$v2x_polyarchy >= dem_threshold ~ TRUE,
+              vdem$v2x_polyarchy[i] >= dem_threshold ~ TRUE,
               TRUE ~ FALSE
               )
+  
+  #call it autocratization if a country is an autocracy this year and was not in prior year
+  vdem$autocratize[i] <- 
+    case_when(pre1_polyarchy < dem_threshold ~ FALSE,
+              vdem$v2x_polyarchy[i] < dem_threshold ~ TRUE,
+              TRUE ~ FALSE
+    )
 }
 
+ ###for countries first appearing in data, determine whether democratized 
+vdem$democratize <- if_else(vdem$first_appear == TRUE,
+                            if_else(vdem$v2x_polyarchy >= dem_threshold,
+                                    TRUE,
+                                    FALSE),
+                            vdem$democratize)
 summary(vdem$democratize)
+
+ ###for countries first appearing in data, determine whether autocratized 
+vdem$autocratize <- if_else(vdem$first_appear == TRUE,
+                            if_else(vdem$v2x_polyarchy < dem_threshold,
+                                    TRUE,
+                                    FALSE),
+                            vdem$autocratize)
+summary(vdem$autocratize)
 
 ####still need to address instance of a country appearing for the first year in the df but as a democracy ####
 
